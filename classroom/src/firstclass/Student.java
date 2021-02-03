5e618bca9f1fc1b44dc68088e4a0fde4774f58ef
subhasish-ghosh-dev <moni.ghos@gmail.com>
2021-02-03 07:18:20 +0000

    "Added Welcome.java"

--- classroom/src/firstclass/Student.java
+++ classroom/src/firstclass/Student.java
@@ -3,7 +3,21 @@ package firstclass;
 public class Student {
 	
 	public int rollNo;
-	
+    public String name;
+    public String semester;
+
+    public Student(){
+
+    }
+
+    public Student(int r, String n, String s){
+        this.rollNo=r;
+        this.name=n;
+        this.semester=s;
+    }
 	
 
+    public void displayInfo(){
+        System.out.println(this.rollNo+" , "+this.name+"  , "+this.semester);
+    }
 }
